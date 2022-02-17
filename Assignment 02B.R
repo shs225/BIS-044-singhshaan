cb842b70b22f8f222e01bf2284d6bbb2ede532b9
Shaan Singh <shs225@lehigh.edu>
2022-02-17 17:34:07 -0500

    Assignment 02B.R

---
+++ Assignment 02B.R
@@ -0,0 +1,20 @@
+x <- 10:1
+y <- -4:5
+z <- c('Hockey', 'Football', 'Curling', 'Soccer', 'Rugby', 'Baseball', 'Golf', 'Basketball', 'Wrestling', 'Tennis')
+theDF <- data.frame(x,y,z)
+theDF
+
+str(theDF)
+
+theDF[1,2]
+View(theDF)
+
+# Using names
+theDF$x
+theDF$x[3]
+
+# Editing names
+names(theDF)<-c("Popularity", "Team Strength", "Sport")
+
+variable = theDF$x
+sd(variable)
